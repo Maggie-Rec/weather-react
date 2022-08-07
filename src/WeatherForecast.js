8d5092be9bb4106e326244f9fb7de730670d2afa
maggie-Rec <marzena.gromo@gmail.com>
2022-08-07 12:01:17 +0200

    added 4 day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,14 +1,18 @@
-import React, { useState } from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState, useEffect, useLayoutEffect } from "react";
+
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
-    console.log(response.data);
     setForecast(response.data.daily);
     setLoaded(true);
   }
@@ -17,21 +21,18 @@ export default function WeatherForecast(props) {
     console.log(forecast);
     return (
       <div className="WeatherForecast">
-        <h3>Next 6 days:</h3>
+        <h3>Next 4 days:</h3>
         <div className="row">
-          <div className="col">
-            {" "}
-            <div className="WeatherForecast-day"> Thu</div>
-            <WeatherIcon code="01d" size={36} />
-            <div className="WeatherForecast-temperatures">
-              <span className="WeatherForecast-temp-max">
-                {forecast[0].temp.max}°
-              </span>
-              <span className="WeatherForecast-temp-min">
-                {forecast[0].temp.min}°
-              </span>
-            </div>
-          </div>
+          {forecast.map(function(dailyForecast, index) {
+            if (index < 4) {
+              return (
+                <div className="col" key={index}>
+                  {" "}
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
